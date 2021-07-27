0b0e6648b9245ecd2b9e8653509ecd46f432606a
구민구 <corleone@kakao.com>
2021-07-28 00:03:55 +0900

    Create text_preprocessing.py

---
+++ preprocessing/text_preprocessing.py
@@ -0,0 +1,69 @@
+'''
+# 21. 07. 27
+'''
+
+def textPreprocessing(txt, method="mecab", stopword=[]):
+    temp = []
+    if method == "mecab":
+        from eunjeon import Mecab
+        mecab = Mecab()
+        txt = mecab.pos(txt)
+
+        for i in txt:
+            if i[1][0] == "J" or i[1][0] == "S":
+                if i[1] == "SL":
+                    continue
+                temp.append(txt.index(i))
+
+    elif method == "okt":
+        from konlpy.tag import Okt
+        okt = Okt()
+        txt = okt.normalize(txt)
+
+        txt = okt.pos(txt)
+
+        for i in txt:
+            if i[1] == "Josa" or i[1] == "Number" or i[1] == "Punctuation":
+                temp.append(txt.index(i))
+    '''
+    for i in txt:
+        if method == "mecab":
+            if i[1][0] == "J" or i[1][0] == "S":
+                if i[1] == "SL":
+                    continue
+                temp.append(txt.index(i))
+        
+        elif method == "okt":
+            if i[1] == "Josa" or i[1] == "Number" or i[1] == "Punctuation":
+                temp.append(txt.index(i))
+    '''
+    temp.reverse()
+
+    for i in temp:
+        del txt[i]
+    
+    result = [i[0] for i in txt]
+
+    for i in stopword:
+        while i in result:
+            result.remove(i)
+
+    return " ".join(result)
+
+def getStopWords():
+    stopword_filename = "stopwords"
+    with open(stopword_filename+'.txt', 'rt', encoding="utf-8") as f:
+        stopword = f.readlines()
+
+    # stopword_list = [ line.strip('\n') for line in stopword ]
+    
+    return [ line.strip('\n') for line in stopword ] # stopword_list
+
+
+
+if __name__ == "__main__":
+    txt = "애플워치를 너무 사고싶다!!"
+    stword = getStopWords()
+    print( "1.", textPreprocessing(txt, method="mecab", stopword=stword) )
+    print( "2.", textPreprocessing(txt, method="okt", stopword=stword) )
+    pass

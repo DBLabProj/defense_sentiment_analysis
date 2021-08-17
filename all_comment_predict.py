bc7ce57f730acc16125e11f1fd1dc278e292e049
구민구 <corleone@kakao.com>
2021-08-17 14:49:14 +0900

    Update all_comment_predict.py

--- all_comment_predict.py
+++ all_comment_predict.py
@@ -13,14 +13,14 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] ='2'
 
 
 # load RNN model
-model_name = "volunteer_model_5"
+model_name = "volunteer_model_try_2"
 rnn_model = tf.keras.models.load_model(
     "./model_save/tf_model/"+ model_name, 
     custom_objects={"TextVectorization":TextVectorization}
     )
 
-path = "./data/" # comment  json file directory path
-jsonfile = "volunteer_all"
+path = "./data/mecab/comment_json/" # comment  json file directory path
+jsonfile = "volunteer_all_mecab"
 with open(path+jsonfile+'.json', encoding="utf-8") as json_file:
     data = json.load(json_file) # load json file
 
@@ -32,14 +32,15 @@ for idx, date in  enumerate( data.keys() ):
         for comment in day_article[article]['comments']:
             if comment=="": # if comment is not include text
                 continue
-            preprocessedData = textPreprocessing(comment.replace('\n',' ').replace('\r',' '),
-                                                 method="mecab", stopword=[])
+            preprocessedData = comment.replace('\n',' ').replace('\r',' ')
+            # preprocessedData = textPreprocessing(comment.replace('\n',' ').replace('\r',' '),
+            #                                      method="mecab", stopword=[])
             
             emotion = float(rnn_model.predict([preprocessedData])) # predict comment sentiment
             emotion = 0 if emotion<0.5 else 1
             data[date][article]['emotions'].append( emotion )
 
-result_path = "./data/predict-data/tf/" #  result save json path
-result_json = "tf_predict_volunteer_all_3"
+result_path = "./data/predict-data/" #  result save json path
+result_json = "tf_volunteer_all_try_2"
 with open(result_path+result_json+".json", "wt", encoding="utf-8") as json_file:
     json.dump(data, json_file, indent="\t", ensure_ascii = False)
